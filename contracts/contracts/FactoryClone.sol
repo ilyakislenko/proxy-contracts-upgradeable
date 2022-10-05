4594ddc00f69e7536c39838bd4aa020f405c89f8
Ilya Kislenko <62322166+cashcats69@users.noreply.github.com>
2022-10-05 22:34:27 +0700

    fix:tokens array fix

--- contracts/contracts/FactoryClone.sol
+++ contracts/contracts/FactoryClone.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.6;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/presets/ERC20PresetFixedSupplyUpgradeable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract FactoryClone {
     address immutable tokenImplementation;
-
+    address [] tokens;
     constructor() {
         tokenImplementation = address(new ERC20PresetFixedSupplyUpgradeable());
     }
-
+    function getTokens(uint256 startIndex, uint256 endIndex) external view returns (address [] memory) {
+        require(endIndex > startIndex, "Bad input");
+        address[] memory tokensAddress = new address[](endIndex - startIndex);
+        uint256 index = 0;
+        for (uint256 i = startIndex; i < endIndex; i++) {
+            tokensAddress[index] = tokens[i];
+            index++;
+        }
+        return tokensAddress;
+    }
     function createToken(
         string calldata name,
         string calldata symbol,
@@ -17,6 +26,7 @@ contract FactoryClone {
     ) external returns (address) {
         address clone = Clones.clone(tokenImplementation);
         ERC20PresetFixedSupplyUpgradeable(clone).initialize(name, symbol, initialSupply, msg.sender);
+        tokens.push(clone);
         return clone;
     }
 }
